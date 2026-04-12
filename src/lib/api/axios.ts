b9ecc3a004a9d2b19aafeabdac623fe58f66d0aa
KaterinaSSlavova <k.slavova@student.fontys.nl>
2026-04-12 21:01:57 +0200

    setting up docker

--- src/lib/api/axios.ts
+++ src/lib/api/axios.ts
@@ -1,9 +1,8 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: "http://localhost:8080/api", // your backend
+    baseURL: import.meta.env.VITE_API_BASE_URL,
     headers: {
         'Content-Type': 'application/json',
     },
 });
-
